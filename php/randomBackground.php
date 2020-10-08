8ee1c4e91b992b970538be0791118a5671c1e010
Rich King <richardrking@pm.me>
2020-10-07 22:28:03 -0700

    Commented out the rebuilt parts

--- php/randomBackground.php
+++ php/randomBackground.php
@@ -2,14 +2,14 @@
 
 	// declare CSS file
 
-	header("Content-type: text/css");
+	// header("Content-type: text/css");
 
 	// select a random background image
 
-	$bg = array('bg-01.jpg', 'bg-03.jpg', 'bg-04.jpg', 'bg-05.jpg', 'bg-06.jpg', 'bg-07.jpg', 'bg-08.jpg', 'bg-09.jpg' );
+	// $bg = array('bg-01.jpg', 'bg-03.jpg', 'bg-04.jpg', 'bg-05.jpg', 'bg-06.jpg', 'bg-07.jpg', 'bg-08.jpg', 'bg-09.jpg' );
 
-	$i = rand(0, count($bg)-1);
-	$selectedBg = "$bg[$i]";
+	// $i = rand(0, count($bg)-1);
+	// $selectedBg = "$bg[$i]";
 
 ?>
 html {

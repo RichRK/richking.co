1ae22ce94ea5559ac1b036c12205c7e1b4b0aa43
Rich King <richardrking@pm.me>
2020-10-08 21:05:57 -0700

    Final changes for new JS background support

--- js/master.js
+++ js/master.js
@@ -8,8 +8,6 @@ var backgrounds = [ 'bg-01.jpg', 'bg-03.jpg', 'bg-04.jpg', 'bg-05.jpg', 'bg-06.j
 
 $( document ).ready( function() {
 
-    // 1. Right now jQuery isn't present on all pages.
-
     injectBackgroundIntoPage( retrieveBackground() );
 });
 
@@ -17,9 +15,7 @@ $( document ).ready( function() {
 
 function injectBackgroundIntoPage( background ) {
 
-    var styling = '';
-
-    // 2. Must define the style above.
+    var styling = 'html { background: -moz-linear-gradient(top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.7) 100%) fixed, url(/images/' + background + ') no-repeat center bottom fixed; background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0.7)), color-stop(100%,rgba(0,0,0,0.7))) fixed, url(/images/' + background + ') no-repeat center bottom fixed; background: -webkit-linear-gradient(top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.7) 100%) fixed, url(/images/' + background + ') no-repeat center bottom fixed; background: -ms-linear-gradient(top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.7) 100%) fixed, url(/images/' + background + ') no-repeat center bottom fixed; background: linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.7) 100%) fixed, url(/images/' + background + ') no-repeat center bottom fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }';
 
     var styleTag = document.createElement( 'style' );
     styleTag.textContent = styling;

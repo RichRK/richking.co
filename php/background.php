ae418f109831e1fe03be9fd8f2b995fccf0f4f51
Rich <richardrking+github@pm.me>
2020-12-29 13:41:27 -0800

    Random background now implemented in JS

--- php/background.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $bg = array('bg-01.jpg', 'bg-02.jpg', 'bg-03.jpg', 'bg-04.jpg', 'bg-05.jpg', 'bg-06.jpg', 'bg-07.jpg', 'bg-08.jpg', 'bg-09.jpg' );
-
-  $i = rand(0, count($bg)-1);
-  $selectedBg = "$bg[$i]";
-?>
